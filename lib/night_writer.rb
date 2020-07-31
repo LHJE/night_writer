afbb110507f799707eadc24b9101a9f3a5ecbd3e
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-31 17:59:52 -0600

    NightWriter exist test passed

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,26 +1,49 @@
 require 'pry'
 
 
-File.open("data/message.txt", "r+") do |file|
+class NightWriter
+  attr_reader :file_reader
 
-  
-
-  file.each do |message|
-    split_message = message.split("")
-    split_message.each do |char|
-
-      binding.pry
-    end
+  def initialize
+    @reader = FileReader.new
   end
 
+  def encode_file_to_braille
+    # I wouldn't worry about testing this method
+    # unless you get everything else done
+    plain = reader.read
+    braille = encode_to_braille(plain)
+  end
+
+  def encode_to_braille(input)
+    # you've taken in an INPUT string
+    # do the magic
+    # send out an OUTPUT string
+  end
 end
 
-File.open("data/braille.txt", "w") do |file|
+puts ARGV.inspect
 
-
-# binding.pry
-
-  # file.write
-
-
-end
+# File.open("data/message.txt", "r+") do |file|
+#
+#
+#
+#   file.each do |message|
+#     split_message = message.split("")
+#     split_message.each do |char|
+#
+#       binding.pry
+#     end
+#   end
+#
+# end
+#
+# File.open("data/braille.txt", "w") do |file|
+#
+#
+# # binding.pry
+#
+#   # file.write
+#
+#
+# end
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,5 +1,14 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/passenger'
-require './lib/vehicle'
-require './lib/drive_in'
+require './lib/night_writer'
+require './lib/file_reader'
+
+class NightWriterTest < Minitest::Test
+
+  def test_it_exists
+    night_writer = NightWriter.new
+
+    assert_instance_of NightWriter, night_writer
+  end
+
+end
