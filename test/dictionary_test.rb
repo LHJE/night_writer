91ac6a473121955abc613c9fed719f6269bfdaa4
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-31 23:49:51 -0600

    experimenting with the Dictionary tests.

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -5,12 +5,16 @@ require './lib/dictionary.rb'
 class DictionaryTest < Minitest::Test
 
   def test_it_exists
-    dictionary = Dictionary.new
+    dictionary = Dictionary.new("./data/braille_dictionary.txt")
 
     assert_instance_of Dictionary, dictionary
   end
 
+  def test_make_dictionary
+    dictionary = Dictionary.new("./data/braille_dictionary.txt")
 
+    assert_equal "asdff", dictionary
+  end
 
 
 end
