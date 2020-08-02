92e7e7ad1d94c87b1e596ba2c7818c4f776c5568
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-01 18:16:12 -0600

    refactoring NightWriter to potentially make NightReader easier.

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -99,6 +99,24 @@ class NightWriterTest < Minitest::Test
     assert_equal ["0.", "..", ".."], night_writer.find_braille_rows_assembled(braille_by_row)
   end
 
+  def test_find_braille_rows_shortened
+    night_writer = NightWriter.new
+    ARGV[0] = "test_input_writer/message_a_for_tests.txt"
+    ARGV[1] = "test_output_writer/braille_a_for_tests.txt"
+    new_text = night_writer.reader.read_first_arg.chomp.split("")
+    braille_message = night_writer.translate_to_braille(new_text)
+    header, *rows = braille_message
+    braille_by_row = header.zip(*rows)
+    braille_rows_assembled = night_writer.find_braille_rows_assembled(braille_by_row)
+
+    assert_equal ["0.", "..", ".."], night_writer.find_braille_rows_shortened(braille_rows_assembled)
+  end
+
+
+
+
+
+
   def test_find_braille_all_one_line
     night_writer = NightWriter.new
     ARGV[0] = "test_input_writer/message_a_for_tests.txt"
