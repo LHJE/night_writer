fff9da4db2574ac445987b9c5fc63fda41b2d61c
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-03 15:09:38 -0600

    Added error message if two arguments not given correctly, with test.

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -1,7 +1,17 @@
 class FileReader
   def read_first_arg
     filename = ARGV[0]
-    File.read("data/#{filename}")
+    other_file = ARGV[1]
+    if filename == nil || other_file == nil || filename[-4..-1] != ".txt" || other_file[-4..-1] != ".txt"
+      puts ""
+      puts "Please put in two arguments after running the file:"
+      puts "1) the file from which you'd like to read"
+      puts "2) the file to which you'd like to write"
+      puts "* the first file must already exist, and end with '.txt'"
+      exit
+    else
+      File.read("data/#{filename}")
+    end
   end
 
   def read_second_arg
